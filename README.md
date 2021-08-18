b061b860841052b3d902bd3b604536af8613c3f9
dennisliuu <dennisliuu@gmail.com>
2021-08-18 11:03:04 +0800

    refactor: update upload method

--- README.md
+++ README.md
@@ -1,32 +1,47 @@
 # NightwatchJS-Remote-File-Upload
+
 Custom NightwatchJS command for file upload using remote selenium grid
 
 ## Dependencies
+
 This works only for NightwatchJS v1.4+
 
 This project requires [adm-zip](https://www.npmjs.com/package/adm-zip)
+
 ```
 npm i adm-zip
 ```
 
 ## Installation
+
 1. Place the folder customCommands in the root of your project
-2. Update NightwatchJ Config:
+2. Update NightwatchJs Config:
+
 ```javascript
   custom_commands_path: ["customCommands"],
 ```
 
 ## Sample Test
+
 ```javascript
 module.exports = {
-  'File Upload Test' : function (browser) {
-    browser
-      .url('https://the-internet.herokuapp.com/upload')
-      .UploadLocalFile("/path_to_file/testfile.txt", "#file-upload")
-      .click('#file-submit')
-      .pause(10000)
-      .end();
-  }
+  "File Upload Test": (browser) => {
+    sessionQueue.push({ sessionId: browser.sessionId, GUID: GUID });
+    setSessionName(browser.sessionId, `${GUID}: test file upload`).then(() => {
+      browser
+        .url("https://the-internet.herokuapp.com/upload")
+        .uploadLocalFile(
+          "/path/to/file",
+          '[data-qa="File-Input"]'
+        )
+        .click("#file-submit")
+        .pause(10000)
+        .end();
+    });
+  },
 };
-
 ```
+
+## Ref:
+
+1. https://github.com/nightwatchjs/nightwatch/issues/890
--- customCommands/UploadLocalFile.js
+++ customCommands/UploadLocalFile.js
@@ -1,69 +1,45 @@
-const AdmZip = require('adm-zip')
+const AdmZip = require('adm-zip');
+const util = require('util');
+const events = require('events');
 
-module.exports = class UploadLocalFile {
-  async command(filePath, inputSelector) {
-    // Get Local File
-    var zip = new AdmZip()
-    zip.addLocalFile(filePath)
-    var file = zip.toBuffer().toString('base64')
-
-    // Upload file to remote terminal
-    try {
-      var returnValue = await this.httpRequest({
-        path: '/session/:sessionId/file',
-        sessionId: this.api.sessionId,
-        data: { file },
-        method: 'POST'
-      })
-    }  catch (err) {
-      console.error('An error occurred uploading file', err)
-      return {
-        status: -1,
-        error: err.message
-      }
-    }
-
-    // Save path of file on remote terminal
-    var tempFilePath = returnValue.value
-
-    // Get file input element
-    try {
-      var returnValue = await this.httpRequest({
-        path: '/session/:sessionId/element',
-        sessionId: this.api.sessionId,
-        data: {
-          using: "css selector",
-          value: inputSelector
-        },
-        method: 'POST'
-      })
-    }  catch (err) {
-      console.error('An error occurred', err)
-      return {
-        status: -1,
-        error: err.message
-      }
-    }
-
-    // Update file input with file path
-    try {
-      var returnValue = await this.httpRequest({
-        path: '/session/:sessionId/element/' + returnValue.value.ELEMENT + '/value',
-        sessionId: this.api.sessionId,
-        data: {
-          value: tempFilePath.split('')
-        },
-        method: 'POST'
-      })
-    }  catch (err) {
-      console.error('An error occurred', err)
-      return {
-        status: -1,
-        error: err.message
-      }
-    }
-
-    // Return response
-    return returnValue
-  }
+function uploadLocalFile() {
+  events.EventEmitter.call(this);
 }
+
+// inherit events class
+util.inherits(uploadLocalFile, events.EventEmitter);
+
+uploadLocalFile.prototype.command = async function commandFn(
+  filePath,
+  inputSelector,
+) {
+  this._stackTrace = commandFn.stackTrace;
+  const self = this;
+  const zip = new AdmZip();
+  zip.addLocalFile(filePath);
+  const file = zip.toBuffer().toString('base64');
+
+  try {
+    var returnValue = await this.httpRequest({
+      path: '/session/:sessionId/file',
+      sessionId: this.api.sessionId,
+      data: { file },
+      method: 'POST',
+    });
+  } catch (err) {
+    console.error('An error occurred uploading file', err);
+    return {
+      status: -1,
+      error: err.message,
+    };
+  }
+
+  // using native api: setValue
+  self.client.api.setValue(inputSelector, returnValue.value, function() {
+    // emit 'complete' when command finished, and continue the testing
+    self.emit('complete');
+  });
+  return this;
+};
+
+module.exports = uploadLocalFile;
